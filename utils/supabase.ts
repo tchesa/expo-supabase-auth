28c980ae51aa5d0c2cb4b5995f0fc3140ef4f594
tchesa <cesarolimpio@gmail.com>
2024-05-16 16:59:23 -0300

    signup with apple id

---
+++ utils/supabase.ts
@@ -0,0 +1,18 @@
+// import "react-native-url-polyfill/auto";
+import { createClient } from "@supabase/supabase-js";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const supabase = createClient(
+  process.env.EXPO_PUBLIC_SUPABASE_URL || "",
+  process.env.EXPO_PUBLIC_SUPABASE_PUBLIC_KEY || "",
+  {
+    auth: {
+      storage: AsyncStorage,
+      autoRefreshToken: true,
+      persistSession: true,
+      detectSessionInUrl: false,
+    },
+  }
+);
+
+export default supabase;
